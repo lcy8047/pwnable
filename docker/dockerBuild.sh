ba45d6159ececc16b70758dcfe6d2ef6eeb2710a
wjsdurtlzuwnj <wjsdurtlzuwnj0701@gmail.com>
2021-07-06 23:59:40 +0900

    dockerBuild script

---
+++ docker/dockerBuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "docker build script : tag = ubuntu20:[tag]"
+# current version image tag print
+echo -en "now version : "
+CURVER=$(docker images | grep pwn |awk '{print $2}')
+echo $CURVER
+if [ $# == 1 ]; then
+	if [ ! -f ./mydocker ]; then
+		echo "myDocker is not Found"
+		exit 1
+	fi
+	echo "Build Docker image?"
+	read INPUT
+	docker build --tag ubuntu20:$1 - < ./myDocker
+else
+	echo -en "wrong tag : "
+	echo $1
+	echo "usage : $0 [tag]"
+	echo "ex) $0 pwn0.1"
+fi
