a154b7e85fb9a46f3cc9749e6fdc6cdac9e6397a
wjsdurtlzuwnj <wjsdurtlzuwnj0701@gmail.com>
2021-07-17 00:15:21 +0900

    add copy .vimrc to /root

--- docker/runDocker.sh
+++ docker/runDocker.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-PWNDIR=/Users/ichang-yul/Desktop/pwnable/pwnable
-docker run --rm -it -v $PWNDIR:/pwnable ubuntu20:pwn0.1 /bin/bash
+
+# set mount dir path
+PWNDIR=/Users/ichang-yul/Desktop/pwnable
+
+# docker run
+echo === docker run ===
+docker run --rm -it -d -v $PWNDIR:/pwnable ubuntu20:pwn0.1 /bin/bash
+
+# get docker container id
+export CONTAINERID=$(docker ps -a | grep pwn | awk '{print $1}')
+echo "docker ID=$CONTAINERID"
+
+# copy .vimrc to root home dir 
+echo === copy .vimrc ===
+docker cp ./etc/.vimrc $(echo -en $CONTAINERID):/root/.vimrc
+
+# attach docker
+echo === attach container ===
+docker attach $(echo -en $CONTAINERID)
