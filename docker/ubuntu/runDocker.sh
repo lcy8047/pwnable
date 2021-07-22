7ee0f151145bd5b312caacc9f3812329f1f6a5f2
lcy8047 <lcy8047@gmail.com>
2021-07-22 10:40:37 +0900

    docker cp command change to remark
    image에서 copy를 하기 때문에 더 이상 필요없어서 주석처리함

--- docker/ubuntu/runDocker.sh
+++ docker/ubuntu/runDocker.sh
@@ -5,16 +5,24 @@ PWNDIR=/Users/ichang-yul/Desktop/pwnable
 
 # docker run
 echo === docker run ===
-docker run --rm -it -d -v $PWNDIR:/pwnable ubuntu20:pwn0.1 /bin/bash
+echo -n "Input tag ex) pwn0.1 : "
+read TAG
+#docker run --rm -it -d -v $PWNDIR:/pwnable ubuntu20:$TAG /bin/bash
+docker run --rm -it -v $PWNDIR:/pwnable ubuntu20:$TAG /bin/bash
+
+
+############################################
+# no more need copy vimrc (it is in image) #
+############################################
 
 # get docker container id
-export CONTAINERID=$(docker ps -a | grep pwn | awk '{print $1}')
-echo "docker ID=$CONTAINERID"
+#export CONTAINERID=$(docker ps -a | grep pwn | awk '{print $1}')
+#echo "docker ID=$CONTAINERID"
 
 # copy .vimrc to root home dir 
-echo === copy .vimrc ===
-docker cp ./etc/.vimrc $(echo -en $CONTAINERID):/root/.vimrc
+#echo === copy .vimrc ===
+#docker cp ./etc/.vimrc $(echo -en $CONTAINERID):/root/.vimrc
 
 # attach docker
-echo === attach container ===
-docker attach $(echo -en $CONTAINERID)
+#echo === attach container ===
+#docker attach $(echo -en $CONTAINERID)
